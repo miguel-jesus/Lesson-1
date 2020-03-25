e45d73bd14502fbf052d49661325aae3c289c3db
miguel-jesus <migueljesuscolom@paucasesnovescifp.cat>
2020-03-25 18:24:49 +0100

    Lesson 1.2
    sin problemas

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //move the vehicle forward
+        //forward = adelante, si ponemos 0,0,1 le indicamos que se mueva en la z hacia adelante
+        //hacemos que vaya mas despacio con el deltaTime
+        transform.Translate(Vector3.forward * Time.deltaTime *20);
+    }
+}
