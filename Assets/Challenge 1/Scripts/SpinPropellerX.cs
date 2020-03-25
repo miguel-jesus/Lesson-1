cb13d97099cef731925ef02ecb7d056913cd44c3
miguel-jesus <migueljesuscolom@paucasesnovescifp.cat>
2020-03-25 19:54:04 +0100

    Challenge 1
    ha sido bastante sencillo solo era repasar los conceptos anteriores y gracias a las pistas ha sido mas sencillo ,lo unico que habia que pensar era en el del spinner que era hacer un script solo para el y decirle que rotara con el vector3.forward y asignarle una velocidad

---
+++ Assets/Challenge 1/Scripts/SpinPropellerX.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX: MonoBehaviour
+{
+    private float turnSpeed = 20.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+         transform.Rotate(Vector3.forward, turnSpeed);
+    }
+}
