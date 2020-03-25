5684bdbe5932b0f1de051a0fbe7a76c559a4fa35
miguel-jesus <migueljesuscolom@paucasesnovescifp.cat>
2020-03-25 19:14:16 +0100

    Lesson 1.4
    sin problema

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20;
+    private float speed = 20;
+    private float turnSpeed = 25.0f; // velocidad de giro
+    private float horizontalInput;
+    private float forwardInput;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");//key a d lados
+        forwardInput = Input.GetAxis("Vertical");//keys w s adelante atras
+
         //move the vehicle forward
         //forward = adelante, si ponemos 0,0,1 le indicamos que se mueva en la z hacia adelante
-        //hacemos que vaya mas despacio con el deltaTime
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        //hacemos que vaya mas despacio con el deltaTime 
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+
+        //en lugar de girar de lado a lado ahora rotaremos ,es decir,un movimiento de giro mas realista
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        //Allow the vehicle to move left/right
+       // transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
